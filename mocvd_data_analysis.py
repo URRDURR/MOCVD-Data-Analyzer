2797d2ff388dd57030979fb129dc6c55c0b1c360
URRDURR <guneet21m@gmail.com>
2024-08-28 12:32:08 -0700

    Cleaned up code at request of pylint. Implemented enumeration, docstrings, and submitted more issues to repo

--- mocvd_data_analysis.py
+++ mocvd_data_analysis.py
@@ -1,16 +1,20 @@
+"""Generates an excel sheet depicting MOCVD source 
+   usage and time opperating when given log files"""
+
 import os
+import re
 from tkinter import filedialog as fd
 import numpy as np
 import pandas as pd
 from scipy import integrate
-import re
 
 STANDARD_PRESSURE_TORR = 760
 STANDARD_TEMPERATURE_KELVIN = 273.15
+R_TORR_LITERS = 62.3638
 
 
-# returns the total number of liters that flow out of the bubbler
 def slpm_to_liters(df, organometal):
+    """Returns the total number of liters that flow out of the bubbler"""
     index_tracker = []
 
     for i in range(len(df["Date"])):
@@ -20,11 +24,15 @@ def slpm_to_liters(df, organometal):
             and df.loc[i, organometal.bubbler_in_valve] == 1
         ):
             index_tracker.append(i)
-    # NOTE: Assumsumption here is that if the valve opens multiple times, the slight increase in total volume calculated due to placing the 1st closing
-    # by the second oppening of the valve, and then integrating over that area, is so negligible that it is unnecessary to change the calculation to remove this
+    # NOTE: Assumsumption here is that if the valve opens multiple times,
+    #  the slight increase in total volume calculated due to placing the 1st closing
+    # by the second oppening of the valve, and then integrating over that area, is so
+    #  negligible that it is unnecessary to change the calculation to remove this
 
     # array of the pressure level (Torr)
-    pressure_array = np.squeeze(np.asarray(df.loc[index_tracker, [organometal.pressure_flow_controler]]))
+    pressure_array = np.squeeze(
+        np.asarray(df.loc[index_tracker, [organometal.pressure_flow_controler]])
+    )
 
     # array of the rate of flow (standard cubic centimeters per minute)
     flow_rate_array = np.squeeze(
@@ -32,12 +40,13 @@ def slpm_to_liters(df, organometal):
     )  # NOTE: this may be wrong in how this is converted/intergrated, please check with David
 
     # array of the rate of flow (standard liters per second)
-    for i in range(len(flow_rate_array)):
-        flow_rate_array[i] /= 60 * 1000
+    for i, flow_rate_index in enumerate(flow_rate_array):
+        flow_rate_index /= 60 * 1000
 
+    # TODO: #7 Just use enumerate from now on it seems way beter
     # Turned into regular liters per second
-    for i in range(len(flow_rate_array)):
-        flow_rate_array[i] = flow_rate_array[i] * (
+    for i, flow_rate_index in enumerate(flow_rate_array):
+        flow_rate_index = flow_rate_index * (
             ((organometal.t + STANDARD_TEMPERATURE_KELVIN) / (STANDARD_TEMPERATURE_KELVIN))
             * (STANDARD_PRESSURE_TORR / (pressure_array[i] - organometal.partial_pressure_torr()))
         )
@@ -48,13 +57,12 @@ def slpm_to_liters(df, organometal):
     return total_liters_flow, index_tracker
 
 
-# Assuming carrier gas is saturated immediately, calculates grams of metal oxide contained
 def liters_to_grams(liters, organometal):
-    # gas constant in units "Torr" and "Liters"
-    R_TORR_LITERS = 62.3638
-
+    """Assuming carrier gas is saturated immediately, calculates grams of metal oxide contained"""
     # Applies (pv)/(rt) = n to get mols
-    mols = (organometal.partial_pressure_torr() * liters) / (R_TORR_LITERS * (organometal.t + STANDARD_TEMPERATURE_KELVIN))
+    mols = (organometal.partial_pressure_torr() * liters) / (
+        R_TORR_LITERS * (organometal.t + STANDARD_TEMPERATURE_KELVIN)
+    )
     grams = organometal.molar_mass * mols
 
     return grams
@@ -62,6 +70,7 @@ def liters_to_grams(liters, organometal):
 
 # give the locations of all files of a type in a folder
 def extract_file_locations(folder_path):
+    """Returns the locations of all files of a given type in the folder path provided"""
     file_type = "csv"
     # variable holds list of locations of all files of one type in a folder
     file_paths = []
@@ -72,15 +81,27 @@ def extract_file_locations(folder_path):
             file_paths.append(i)
 
     # give absolute path of each file in the directory
-    for i in range(len(file_paths)):
-        file_paths[i] = folder_path + "\\" + file_paths[i]
+    for i,file_path in enumerate(file_paths):
+        file_path = folder_path + "\\" + file_path
 
     return file_paths
 
 
-class Organometal_Source:
+class OrganometalSource:
+    """Class representing an organometal source"""
+
     def __init__(
-        self, name, bubbler_in_valve, bubbler_out_valve, blocking_valve, mass_flow_controler, pressure_flow_controler, molar_mass, a, b, t
+        self,
+        name,
+        bubbler_in_valve,
+        bubbler_out_valve,
+        blocking_valve,
+        mass_flow_controler,
+        pressure_flow_controler,
+        molar_mass,
+        a,
+        b,
+        t,
     ):
         self.name = name
         self.bubbler_in_valve = bubbler_in_valve
@@ -96,9 +117,11 @@ class Organometal_Source:
         self.time_per_run = []
 
     def partial_pressure_torr(self):
+        """Calculates the partial presure"""
         return 10 ** (self.a - (self.b / (self.t + STANDARD_TEMPERATURE_KELVIN)))
 
     def calculate_mass_and_time(self, df):
+        """Returns the total grams of source used and seconds of time opperating during 1 run/log file"""
         total_liters, index_tracker = slpm_to_liters(df, self)
 
         grams = float(liters_to_grams(total_liters, self))
@@ -107,7 +130,7 @@ class Organometal_Source:
         return grams, time
 
 
-tmal = Organometal_Source(
+tmal = OrganometalSource(
     name="TMAl",
     bubbler_in_valve="DO40",
     bubbler_out_valve="DO38",
@@ -120,7 +143,7 @@ tmal = Organometal_Source(
     t=-10,
 )
 
-tmga = Organometal_Source(
+tmga = OrganometalSource(
     name="TMGa",
     bubbler_in_valve="DO28",
     bubbler_out_valve="DO26",
@@ -135,7 +158,7 @@ tmga = Organometal_Source(
 
 PATH_VIA_TERMINAL = True
 
-test_amount = 0.009555694
+TEST_AMOUNT = 0.009555694
 
 # Selects all the files to be read
 if PATH_VIA_TERMINAL:
@@ -152,8 +175,8 @@ if PATH_VIA_TERMINAL:
         else:
             print("please try again")
 else:
-    file_path = r"C:/Users/gma78/Desktop/Excels/2024-06-28_S258_Datalog 6-28-2024 3-47-03 PM.csv"
-    files = [file_path]
+    FILE_PATH = r"C:/Users/gma78/Desktop/Excels/2024-06-28_S258_Datalog 6-28-2024 3-47-03 PM.csv"
+    files = [FILE_PATH]
 
 # TODO: #2 Add interface for selecting organometals and creating file title
 organometals_list = [tmal, tmga]
